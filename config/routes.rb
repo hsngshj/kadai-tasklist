e82e466c1f531f96ae2a8dd461f2f842dec6257b
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2022-12-30 05:39:26 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/new'
-  # get 'users/create'
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
-  resources :users, only: [:index, :show, :create]
 end
