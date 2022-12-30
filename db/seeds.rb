b3c2518a389e9eeea8f0847e2e297dd6025d8090
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2022-12-30 07:39:09 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'taro', email: 'taro@techacademy.jp', password: 'taro1234')
-User.create(name: 'hsngshj', email: 'standbyme1212@gmail.com', password: 'hsngshj1234')
+# User.create(name: 'taro', email: 'taro@techacademy.jp', password: 'taro1234')
+# User.create(name: 'hsngshj', email: 'standbyme1212@gmail.com', password: 'hsngshj1234')
 
-(1..100).each do |number|
-  Task.create(user_id: 1, status: 'status ' + number.to_s, content: 'test content ' + number.to_s)
-end
+# (1..100).each do |number|
+#   Task.create(user_id: 1, status: 'status ' + number.to_s, content: 'test content ' + number.to_s)
+# end
 
-(1..100).each do |number|
-  Task.create(user_id: 2, status: 'status ' + number.to_s, content: 'test content ' + number.to_s)
-end
+# (1..100).each do |number|
+#   Task.create(user_id: 2, status: 'status ' + number.to_s, content: 'test content ' + number.to_s)
+# end
