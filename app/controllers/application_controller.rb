913dfcc497cdce39ccdb1e38abeca3e39c41f978
Ubuntu <ubuntu@ip-172-31-12-101.ap-northeast-1.compute.internal>
2023-01-04 08:14:47 +0000

    reivise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
     
     def require_user_logged_in
         unless logged_in?
-           redirect_to login_url 
+           redirect_to login_url
         end
-    end
-    
-    def counts(user)
-       @count_tasks = user.tasks.count 
     end
 end
